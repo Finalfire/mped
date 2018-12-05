eb509ab75334c107e6203da42cd2177d9c2661e8
Luca Cinelli <lucacinelli.1995@gmail.com>
2018-12-05 18:14:43 +0100

    only Heuristic class

---
+++ src/heuristic/Heuristic.h
@@ -0,0 +1,22 @@
+//
+// Created by lucac on 04/12/2018.
+//
+
+#ifndef MPED_HEURISTIC_H
+#define MPED_HEURISTIC_H
+
+#include"../metric/Metric.h"
+
+class Heuristic {
+protected:
+    Metric& metric;
+
+public:
+    explicit Heuristic(Metric& m):metric(m){};
+
+    virtual unsigned compute_heuristic(const AbstractSequence&, const AbstractSequence&) {};
+
+};
+
+
+#endif //MPED_HEURISTIC_H
